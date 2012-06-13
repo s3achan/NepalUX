cf4d3a754082f73b99fdecfb2ac1c7909e162ad7
Shikshya <shikshyabhattachan@gmail.com>
2012-06-12 23:33:37 -0400

    merged the changes

---
+++ NepalUX/src/com/milanix/nepalux/content/Gallery.java
@@ -0,0 +1,14 @@
+package com.milanix.nepalux.content;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class Gallery extends Activity implements AdapterView.OnItemClickListener{
+
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
